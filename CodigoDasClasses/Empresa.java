7b9a8f5d28bbb2f90b4af3096c33ff87c7ed9a21
Ruan Nicolini <ruannicolini@gmail.com>
2014-04-14 14:36:49 -0300

    Alteração no código das Classes

--- CodigoDasClasses/Empresa.java
+++ CodigoDasClasses/Empresa.java
@@ -1,4 +1,3 @@
-
 package agenda;
 
 public class Empresa {
@@ -6,5 +5,31 @@ public class Empresa {
     private String nome;
 
     private Endereco endereco;
+
+    /* ==========CONSTRUTOR==========*/
+    public Empresa(String nome, Endereco endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+      
+    /* ==========Get And Set==========*/
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
     
+    
+    /*==========METODOS==========*/
+
 }
--- CodigoDasClasses/Operadora.java
+++ CodigoDasClasses/Operadora.java
@@ -1,7 +1,48 @@
 package agenda;
 
 public class Operadora {
-    
+    private int idOperadora;
     private String nome;
+    private String codOperadora;
+    
+    /* ==========CONSTRUTOR==========*/
+    public Operadora(String nome) {
+        this.nome = nome;
+    }
+
+    public Operadora(int idOperadora, String nome) {
+        this.idOperadora = idOperadora;
+        this.nome = nome;
+    }
+
+    
+    /* ==========Get And Set==========*/
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public int getIdOperadora() {
+        return idOperadora;
+    }
+
+    public void setIdOperadora(int idOperadora) {
+        this.idOperadora = idOperadora;
+    }
+    
+    public String getCodOperadora() {
+        return codOperadora;
+    }
+
+    public void setCodOperadora(String codOperadora) {
+        this.codOperadora = codOperadora;
+    }
+    
+    /*==========METODOS==========*/
+
+    
     
 }
